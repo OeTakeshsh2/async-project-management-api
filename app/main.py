d687b5e4074a61d76c373338422ed3011f68564a
Tu Nombre <db0nqfg@gmail.com>
2026-03-26 16:31:03 -0300

    /me endpoint fixed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.core.database import engine, Base
@@ -6,11 +7,21 @@ from app.routes import router
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    # startup
-    async with engine.begin() as conn:
-       # Base contiene el metadata de todos los modelos ORM
-       #y se usa para crear las tablas en la base de datos
-        await conn.run_sync(Base.metadata.create_all)
+
+    for attempt in range(5):
+        # startup
+        try:
+            async with engine.begin() as conn:
+                # base contiene el metadata de todos los modelos orm
+                #y se usa para crear las tablas en la base de datos
+                await conn.run_sync(Base.metadata.create_all)
+                print ('db connected')
+                break
+        except Exception as e:
+            print(f"db not ready yet (attempt:{attempt + 1}): {e}")
+            await asyncio.sleep(2)
+        else:
+            print("could not connect to the database")
     yield
     # shutdown (opcional)
 
